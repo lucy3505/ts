c588d3fad0164111e9c42bc19b5da3d459c40c3f
xyy <xyy@xyydeMac-Studio.local>
2023-05-28 16:52:52 +0800

    update

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-// var myArr: string[] = ["xxx", "uuu", "ccc"]; //数组里都是string
-var myArr = [1, 2, 3, 4];
+// let my_tuple:[string,number];
+//顺序不能颠倒，必须完全符合格式，只写一个string也不行。写3个也不行
+var my_tuple = ["trails265", 100];
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
-// var myArr: string[] = ["xxx", "uuu", "ccc"]; //数组里都是string
+// let my_tuple:[string,number];
+//顺序不能颠倒，必须完全符合格式，只写一个string也不行。写3个也不行
 
-let myArr: Array<number> = [1, 2, 3, 4];
+let my_tuple: [string, number] = ["trails265", 100];
