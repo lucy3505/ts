907b4f177bf78077d98c34ffc21ac7edb7b5ebbe
xyy <xyy@xyydeMac-Studio.local>
2023-05-28 16:44:24 +0800

    update

--- index.ts
+++ index.ts
@@ -1,19 +1,3 @@
-if (true) {
-  let a = 1;
-}
-console.log(a); //使用var 这里就不会报错，ts就不会报错，用let,ts就会报错
+// var myArr: string[] = ["xxx", "uuu", "ccc"]; //数组里都是string
 
-//定义类型和赋值:var 就可以重复定义相同名字变量，但不能改类型
-var my_name: string = "rails3644";
-console.log("my name is" + my_name);
-var my_name: string = "hfpp2012";
-console.log(`my name is ${my_name}`);
-var my_name: number = 123;
-console.log(`my name is ${my_name}`);
-
-//定义类型和赋值:let 不可以重复定义相同名字变量 ts编译会给我们报错
-let my_name1: string = "rails3644";
-console.log("my name is" + my_name1);
-let my_name1: string = "hfpp2012"; // 只能my_name1 = "hfpp2012";
-console.log(`my name is ${my_name1}`);
-//const 就是都不能改变
+let myArr: Array<number> = [1, 2, 3, 4];
