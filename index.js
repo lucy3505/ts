1f3a89cac70386978de4af06d962718e80fd2b36
xyy <xyy@xyydeMac-Studio.local>
2023-05-29 16:31:46 +0800

    update

--- index.js
+++ index.js
@@ -1,102 +1,33 @@
-//模版
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+//函数
+// 只要传过来的参数 o 有name 属性
+// o 可以是一个接口类型，这个接口类型要有name 属性
+//定义o为接口类型，可以传入一个对象，如果不用接口类型，sayName(‘122’);编译不会报错，只有运行的时候才能抱错
+var sayName = function (o) {
+    o.print(o.name);
+};
+//对象
+var person = {
+    age: 27,
+    name: "hfpp2012",
+    print: function (name) {
+        console.log(name);
+    },
+};
+sayName(person);
+// const bottle = {
+//   litres: 1,
+//   name: "biaoliu",
+// };
 var Person = /** @class */ (function () {
-    function Person(firstName, lastName) {
-        this.name = "123";
-        this.firstName = firstName;
-        this.lastName = lastName;
+    function Person() {
     }
-    Person.getStaticAge = function () {
-        return "my age is ".concat(Person.age);
+    Person.prototype.print = function () {
+        //不传参也是可以的
+        console.log(this.name);
     };
-    Person.prototype.getAge = function () {
-        return "my age is ".concat(Person.age);
-    };
-    Person.all = function () {
-        //Person对应数据库的一个表模型
-        //可以列出persons表的所有记录
-        //new出来的可能是一条记录，要找到所有记录，可能要通过静态方法
-        // return Person.select().all()
-    };
-    Person.prototype.sayHi = function () {
-        console.log("private sayHi");
-    };
-    Person.prototype.callSayHi = function () {
-        this.sayHi();
-        console.log("*****");
-    };
-    //public
-    //set 方法
-    Person.prototype.setFirstName = function (firstName) {
-        this.firstName = firstName;
-    };
-    Person.prototype.getFirstName = function () {
-        return this.firstName;
-    };
-    Person.prototype.getLastName = function () {
-        return this.lastName;
-    };
-    Person.prototype.greet = function () {
-        console.log("hi");
-    };
-    Person.prototype.othergreet = function () {
-        this.greet();
-        console.log("*******");
-    };
-    //静态属性 static
-    Person.age = 10;
     return Person;
 }());
-// console.log(Person.age);
-var aPerson = new Person("rail", "365"); //Constructor of class 'Person' is protected and only accessible within the class declaration.
-console.log(aPerson.getAge());
-//public的getStaticAge来调用protected 的static age,如果是private，那不能访问
-console.log(Person.getStaticAge());
-console.log(aPerson.name);
-aPerson.name = "222"; //Cannot assign to 'name' because it is a read-only property.
-var Programmer = /** @class */ (function (_super) {
-    __extends(Programmer, _super);
-    function Programmer(firstName, lastName) {
-        var _this = _super.call(this, firstName, lastName) || this;
-        console.log("Programmer constructor");
-        return _this;
-    }
-    Programmer.prototype.greet = function () {
-        console.log("heelo world");
-    };
-    Programmer.getSuperAge = function () {
-        return "super age is ".concat(Person.age);
-    };
-    Programmer.prototype.greetLikeNormalPeople = function () {
-        _super.prototype.greet.call(this);
-    };
-    //子类定义的方法
-    Programmer.prototype.getFullName = function () {
-        // return `${this.firstName} ${this.lastName}`;
-    };
-    Programmer.prototype.getFullName1 = function () {
-        return "".concat(this.getFirstName(), " ").concat(this.getLastName());
-    };
-    return Programmer;
-}(Person));
-var aProgrammer = new Programmer("rail", "365 ");
-// Constructor of class 'Person' is protected and only accessible within the class declaration
-//需要在class Programmer constructor里进行重写，父类的constructor 是protected的时候可以重写，是private 的时候不能重写
-//是protected的时候，父类不能new,子类在重写constructor后能new
-console.log(Programmer.age); //Property 'age' is protected and only accessible within class 'Person' and its subclasses.
-console.log(Programmer.getStaticAge());
-console.log(Programmer.getSuperAge());
+var aPerson = new Person();
+// aPerson.name = "rails365";
+// sayName(aPerson);
+// sayName(bottle);
--- index.ts
+++ index.ts
@@ -1,109 +1,47 @@
-//模版
+//接口
+//传过来的参数必须包含接口的属性和方法
+interface Named {
+  //属性
+  name: string;
+  //方法
+  //没有方法体的
+  //具体的对象中实现方法体
+  print(name: string): void;
+}
+
+//函数
+// 只要传过来的参数 o 有name 属性
+// o 可以是一个接口类型，这个接口类型要有name 属性
+//定义o为接口类型，可以传入一个对象，如果不用接口类型，sayName(‘122’);编译不会报错，只有运行的时候才能抱错
+const sayName = (o: Named) => {
+  o.print(o.name);
+};
+
+//对象
+const person = {
+  age: 27,
+  name: "hfpp2012",
+  print: (name) => {
+    console.log(name);
+  },
+};
+sayName(person);
+// const bottle = {
+//   litres: 1,
+//   name: "biaoliu",
+// };
 
 class Person {
-  //定义了两个数据
-  private firstName: string; //对类型进行限制
-  protected lastName;
-
-  //静态属性 static
-  protected static age: number = 10;
-
-  readonly name: string = "123";
-
-  public constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  public static getStaticAge() {
-    return `my age is ${Person.age}`;
-  }
-
-  getAge() {
-    return `my age is ${Person.age}`;
-  }
-
-  static all() {
-    //Person对应数据库的一个表模型
-    //可以列出persons表的所有记录
-    //new出来的可能是一条记录，要找到所有记录，可能要通过静态方法
-    // return Person.select().all()
-  }
-  private sayHi() {
-    console.log("private sayHi");
-  }
-
-  public callSayHi() {
-    this.sayHi();
-    console.log("*****");
-  }
-
-  //public
-  //set 方法
-  setFirstName(firstName) {
-    this.firstName = firstName;
-  }
-
-  protected getFirstName() {
-    return this.firstName;
-  }
-
-  getLastName() {
-    return this.lastName;
-  }
-
-  public greet() {
-    console.log("hi");
-  }
-
-  public othergreet() {
-    this.greet();
-    console.log("*******");
+  name: string;
+  print(): void {
+    //不传参也是可以的
+    console.log(this.name);
   }
 }
 
-// console.log(Person.age);
+let aPerson = new Person();
+// aPerson.name = "rails365";
 
-let aPerson = new Person("rail", "365"); //Constructor of class 'Person' is protected and only accessible within the class declaration.
-console.log(aPerson.getAge());
+// sayName(aPerson);
 
-//public的getStaticAge来调用protected 的static age,如果是private，那不能访问
-console.log(Person.getStaticAge());
-console.log(aPerson.name);
-aPerson.name = "222"; //Cannot assign to 'name' because it is a read-only property.
-class Programmer extends Person {
-  constructor(firstName, lastName) {
-    super(firstName, lastName);
-    console.log("Programmer constructor");
-  }
-
-  public greet(): void {
-    console.log("heelo world");
-  }
-
-  public static getSuperAge() {
-    return `super age is ${Person.age}`;
-  }
-
-  public greetLikeNormalPeople() {
-    super.greet();
-  }
-
-  //子类定义的方法
-  getFullName() {
-    // return `${this.firstName} ${this.lastName}`;
-  }
-
-  getFullName1() {
-    return `${this.getFirstName()} ${this.getLastName()}`;
-  }
-}
-let aProgrammer = new Programmer("rail", "365 ");
-
-// Constructor of class 'Person' is protected and only accessible within the class declaration
-//需要在class Programmer constructor里进行重写，父类的constructor 是protected的时候可以重写，是private 的时候不能重写
-//是protected的时候，父类不能new,子类在重写constructor后能new
-
-console.log(Programmer.age); //Property 'age' is protected and only accessible within class 'Person' and its subclasses.
-console.log(Programmer.getStaticAge());
-console.log(Programmer.getSuperAge());
+// sayName(bottle);
