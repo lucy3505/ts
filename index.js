4494ac94cbbb3a8f56ac7b15f2f5b939fb2acf9c
xyy <xyy@xyydeMac-Studio.local>
2023-05-29 13:18:48 +0800

    update

--- index.js
+++ index.js
@@ -16,7 +16,27 @@ var __extends = (this && this.__extends) || (function () {
 })();
 var Person = /** @class */ (function () {
     function Person() {
+        this.firstName = "rials";
+        this.lastName = "hrrp";
     }
+    Person.prototype.sayHi = function () {
+        console.log("private sayHi");
+    };
+    Person.prototype.callSayHi = function () {
+        this.sayHi();
+        console.log("*****");
+    };
+    //public
+    //set 方法
+    Person.prototype.setFirstName = function (firstName) {
+        this.firstName = firstName;
+    };
+    Person.prototype.getFirstName = function () {
+        return this.firstName;
+    };
+    Person.prototype.getLastName = function () {
+        return this.lastName;
+    };
     Person.prototype.greet = function () {
         console.log("hi");
     };
@@ -26,29 +46,37 @@ var Person = /** @class */ (function () {
     };
     return Person;
 }());
-//继承父类的数据和行为，就是属性和方法
-//它的父类就是Person
+var aPerson = new Person();
+// aPerson.lastName = "jj";//Property 'lastName' is protected and only accessible within class 'Person' and its subclasses.
+// console.log(aPerson.firstName); //Property 'firstName' is private and only accessible within class 'Person'
+aPerson.setFirstName("rails");
+aPerson.getFirstName();
+aPerson.callSayHi();
 var Programmer = /** @class */ (function (_super) {
     __extends(Programmer, _super);
     function Programmer() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Programmer.prototype.greet = function () {
-        console.log("hello world");
+        console.log("heelo world");
     };
     Programmer.prototype.greetLikeNormalPeople = function () {
-        this.greet();
-        //super代表父类
         _super.prototype.greet.call(this);
+    };
+    //子类定义的方法
+    Programmer.prototype.getFullName = function () {
+        return "".concat(this.firstName, " ").concat(this.lastName);
+    };
+    Programmer.prototype.getFullName1 = function () {
+        return "".concat(this.getFirstName(), " ").concat(this.getLastName());
     };
     return Programmer;
 }(Person));
-var aProgrammer = new Programmer(); //这里aProgrammer类型可以用Programmer,因为是Programmer new 出来的
-aProgrammer.greet();
-aProgrammer.greetLikeNormalPeople();
-var aProgrammer2 = new Programmer(); //这里aProgrammer2类型可以用Person,但是使用的时候就只能用Person里的内容，否则编译报错
-aProgrammer2.greet();
-aProgrammer2.greetLikeNormalPeople(); //
-aProgrammer2.othergreet(); //
-var aProgrammer3 = new Programmer(); //这里aProgrammer3类型用Object也不报错，但用string是报错的，因为Object是Programmer的父类
-var aProgrammer4 = new Person(); //这样也不行，Programmer这个得是Person的父类才可以
+var aProgrammer = new Programmer();
+//继承的也是不能访问私有属性
+console.log(aProgrammer.firstName); //Property 'firstName' is private and only accessible within class 'Person'
+//继承过来 的方法
+console.log(aProgrammer.getFirstName()); //Property 'firstName' is private and only accessible within class 'Person'
+console.log(aProgrammer.lastName); //Property 'lastName' is protected and only accessible within class 'Person' and its subclasses.
+console.log(aProgrammer.getFullName());
+console.log(aProgrammer.getFullName1());
--- index.ts
+++ index.ts
@@ -2,42 +2,79 @@
 
 class Person {
   //定义了两个数据
-  firstName: string; //对类型进行限制
-  lastName;
+  private firstName: string; //对类型进行限制
+  protected lastName;
 
-  greet() {
+  constructor() {
+    this.firstName = "rials";
+    this.lastName = "hrrp";
+  }
+  private sayHi() {
+    console.log("private sayHi");
+  }
+
+  public callSayHi() {
+    this.sayHi();
+    console.log("*****");
+  }
+
+  //public
+  //set 方法
+  setFirstName(firstName) {
+    this.firstName = firstName;
+  }
+
+  protected getFirstName() {
+    return this.firstName;
+  }
+
+  getLastName() {
+    return this.lastName;
+  }
+
+  public greet() {
     console.log("hi");
   }
 
-  othergreet() {
+  public othergreet() {
     this.greet();
     console.log("*******");
   }
 }
 
-//继承父类的数据和行为，就是属性和方法
-//它的父类就是Person
+let aPerson = new Person();
+// aPerson.lastName = "jj";//Property 'lastName' is protected and only accessible within class 'Person' and its subclasses.
+// console.log(aPerson.firstName); //Property 'firstName' is private and only accessible within class 'Person'
+aPerson.setFirstName("rails");
+aPerson.getFirstName();
+aPerson.callSayHi();
+
 class Programmer extends Person {
-  greet(): void {
-    console.log("hello world");
+  public greet(): void {
+    console.log("heelo world");
   }
 
-  greetLikeNormalPeople() {
-    this.greet();
-    //super代表父类
+  public greetLikeNormalPeople() {
     super.greet();
+  }
+
+  //子类定义的方法
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getFullName1() {
+    return `${this.getFirstName()} ${this.getLastName()}`;
   }
 }
 
-let aProgrammer: Programmer = new Programmer(); //这里aProgrammer类型可以用Programmer,因为是Programmer new 出来的
-aProgrammer.greet();
-aProgrammer.greetLikeNormalPeople();
+let aProgrammer = new Programmer();
+//继承的也是不能访问私有属性
+console.log(aProgrammer.firstName); //Property 'firstName' is private and only accessible within class 'Person'
 
-let aProgrammer2: Person = new Programmer(); //这里aProgrammer2类型可以用Person,但是使用的时候就只能用Person里的内容，否则编译报错
-aProgrammer2.greet();
-aProgrammer2.greetLikeNormalPeople(); //
-aProgrammer2.othergreet(); //
+//继承过来 的方法
+console.log(aProgrammer.getFirstName()); //Property 'firstName' is private and only accessible within class 'Person'
 
-let aProgrammer3: Object = new Programmer(); //这里aProgrammer3类型用Object也不报错，但用string是报错的，因为Object是Programmer的父类
-
-let aProgrammer4: Programmer = new Person(); //这样也不行，Programmer这个得是Person的父类才可以
+console.log(aProgrammer.lastName); //Property 'lastName' is protected and only accessible within class 'Person' and its subclasses.
+console.log(aProgrammer.getFullName());
+console.log(aProgrammer.getFullName1());
