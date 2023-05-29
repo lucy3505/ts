a79e625fbda0ca689467c85ec18dc6c38054305c
xyy <xyy@xyydeMac-Studio.local>
2023-05-29 22:46:14 +0800

    update

--- index.js
+++ index.js
@@ -1,60 +1,31 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Component = /** @class */ (function () {
-    function Component(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-    Component.prototype.display = function () {
-        console.log("displaying");
-        console.log(this.height);
-    };
-    return Component;
-}());
-var Button = /** @class */ (function (_super) {
-    __extends(Button, _super);
-    function Button() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    //如果没有extends Component那就要实现Component里的内容  比如写成class Button implements Widget
-    Button.prototype.hide = function () {
-        console.log("hiding");
-    };
-    Button.prototype.display = function () {
-        console.log("ddd");
-    };
-    return Button;
-}(Component));
-var w = new Button(1, 2);
-console.log(w);
-w.display();
-var Button2 = /** @class */ (function () {
-    function Button2(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-    Button2.prototype.display = function () {
-        console.log("displaying");
-        console.log(this.height);
-    };
-    Button2.prototype.hide = function () {
-        console.log("hiding");
-    };
-    return Button2;
-}());
-var w2 = new Button2(1, 2);
-console.log(w2);
-w2.display();
+var s = { enabled: true, maximized: false, aaa: "dfd" }; //aaa必须是字符串
+var s1 = [true, false]; //数组的索引前面是number
+console.log(s);
+console.log(s["enabled"]);
+var s2 = [true, false]; //如果 [index:number]这个是number的话，就可以是个数组
+var s23 = { 1: true, 2: false }; //如果 [index:number]这个是number的话，就可以是个数组
+console.log(s2);
+console.log(s2[1]);
+console.log(s2.length);
+var s22 = [1, 2, 3];
+console.log(s22.length);
+var s33 = [true, false, false, true];
+console.log(s33);
+console.log(s33.length);
+console.log(s33.pop());
+var s4 = { mainScreen: true, max: false };
+var example = {
+    color: "red",
+    nest: {
+        ".subclass": {
+            color: "blue",
+            nest: {
+                ".sub": { color: "blue" },
+            },
+        },
+    },
+};
+var c1 = {
+    color: "Red",
+};
+// root -> tree
--- index.ts
+++ index.ts
@@ -1,57 +1,76 @@
-class Component {
-  private width: number;
-  protected height: number;
-
-  constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
-  }
-
-  display(): void {
-    console.log("displaying");
-    console.log(this.height);
-  }
-}
-//接口继承类
-//继承类的方法和属性，但可以不用实现方法
-//也可以定义自己的方法
-interface Widget extends Component {
-  hide(): void;
+interface States {
+  [state: string]: boolean; //state可以任意名称，只要类型对应上
 }
 
-class Button extends Component implements Widget {
-  //如果没有extends Component那就要实现Component里的内容  比如写成class Button implements Widget
-  hide(): void {
-    console.log("hiding");
-  }
-  display(): void {
-    console.log("ddd");
-  }
+let s: States = { enabled: true, maximized: false, aaa: "dfd" }; //aaa必须是字符串
+let s1: States = [true, false]; //数组的索引前面是number
+console.log(s);
+console.log(s["enabled"]);
+
+interface State1 {
+  //没有array的方法push,join，length...
+  [index: number]: boolean;
 }
 
-let w: Widget = new Button(1, 2);
-console.log(w);
-w.display();
+let s2: State1 = [true, false]; //如果 [index:number]这个是number的话，就可以是个数组
+let s23: State1 = { 1: true, 2: false }; //如果 [index:number]这个是number的话，就可以是个数组
+console.log(s2);
+console.log(s2[1]);
+console.log(s2.length);
 
-class Button2 implements Widget {
-  //如果没有extends Component那就要实现Component里的内容  比如写成class Button implements Widget
-  //Component里面是private，protected那就只能extends Component,如果是public那就能自己实现width，height
-  private width: number;
-  protected height: number;
+let s22: number[] = [1, 2, 3];
+console.log(s22.length);
+//?这个和interface State1有什么区别
+//1. interface State1可以是对象，可以是数组，比较灵活
+//2. interface State1没有length,s22有length，s2没有array的方法push,join...，s22有
 
-  constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
-  }
-  display(): void {
-    console.log("displaying");
-    console.log(this.height);
-  }
-
-  hide(): void {
-    console.log("hiding");
-  }
+//An index signature parameter type must be 'string', 'number', 'symbol', or a template literal type.
+interface State2 {
+  [index: boolean]: boolean; //index这里的类型永远是 'string', 'number', 'symbol'，不可能是boolean
 }
-let w2: Widget = new Button2(1, 2);
-console.log(w2);
-w2.display();
+
+interface States3 {
+  [index: number]: boolean;
+  length: number;
+  pop(): boolean | undefined;
+}
+
+let s33: States3 = [true, false, false, true];
+console.log(s33);
+console.log(s33.length);
+console.log(s33.pop());
+
+interface State4 {
+  mainScreen: string;
+  [state: string]: boolean;
+  // mainScreen:boolean//必须和上面一样是boolean,否则抱错
+  // mainScreen:strmainScreen:stringing//mainScreen和[state:string]声明的类型一定要相同
+}
+
+let s4: State4 = { mainScreen: true, max: false };
+
+//! 定义一些复杂结构
+interface NestedCss {
+  color?: string;
+  nest?: {
+    [selector: string]: NestedCss;
+  };
+}
+
+let example: NestedCss = {
+  color: "red",
+  nest: {
+    ".subclass": {
+      color: "blue",
+      nest: {
+        ".sub": { color: "blue" },
+      },
+    },
+  },
+};
+
+let c1: NestedCss = {
+  color: "Red",
+};
+
+// root -> tree
