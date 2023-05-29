51cd089d2604b634e913559f56f49f956514f957
xyy <xyy@xyydeMac-Studio.local>
2023-05-29 23:34:47 +0800

    update

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-//不能实例化,要用另一个类去继承它
 var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
+    function Person(name, age) {
+        this._name = name;
+        this._age = age;
     }
-    Person.prototype.display = function () {
-        console.log(this.name);
-    };
     return Person;
 }());
-var p = new Person("xxx"); // Cannot create an instance of an abstract class.
-var Employee = /** @class */ (function (_super) {
-    __extends(Employee, _super);
-    function Employee(name, code) {
-        var _this = _super.call(this, name) || this;
-        _this.subName = name;
-        _this.empCode = code;
-        return _this;
-    }
-    Employee.prototype.find = function (name) {
-        return new Employee(name, 1);
-    };
-    return Employee;
-}(Person));
-var p1 = new Employee("xxx", 12);
-var p2 = p1.find("rails");
-console.log(p2);
+var p = new Person("nnn", 12);
+console.log(p);
+var Person1 = /** @class */ (function () {
+    //这个写法和Person一样，不用写 this._name = name; this._age = age;
+    function Person1(_name, _age) {
+        this._name = _name;
+        this._age = _age;
+    } //
+    return Person1;
+}());
+var p1 = new Person1("nnn", 12);
+console.log(p1);
--- index.ts
+++ index.ts
@@ -1,39 +1,19 @@
-//不能实例化,要用另一个类去继承它
-abstract class Person {
-  abstract subName: string;
-  name: string;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  display(): void {
-    console.log(this.name);
-  }
-
-  //抽象的方法
-  //没有方法体
-  //继承这个抽象类的时候，一定要去实现这个抽象方法
-  //可加public private，protected控制符
-  public abstract find(string): Person;
-}
-
-let p: Person = new Person("xxx"); // Cannot create an instance of an abstract class.
-
-class Employee extends Person {
-  empCode: number;
-  subName: string;
-  constructor(name: string, code: number) {
-    super(name); //必须调用super
-    this.subName = name;
-    this.empCode = code;
-  }
-
-  public find(name: string): Person {
-    return new Employee(name, 1);
+class Person {
+  private _name: string;
+  private _age: number;
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
   }
 }
 
-let p1: Person = new Employee("xxx", 12);
-let p2: Person = p1.find("rails");
-console.log(p2);
+let p: Person = new Person("nnn", 12);
+console.log(p);
+
+class Person1 {
+  //这个写法和Person一样，不用写 this._name = name; this._age = age;
+
+  constructor(private _name: string, private _age: number) {} //
+}
+let p1: Person1 = new Person1("nnn", 12);
+console.log(p1);
