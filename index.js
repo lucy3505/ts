ba2c6fef92d1e593b0568e4369b66d91b2893732
xyy <xyy@xyydeMac-Studio.local>
2023-05-29 22:52:00 +0800

    update

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-var s = { enabled: true, maximized: false, aaa: "dfd" }; //aaa必须是字符串
-var s1 = [true, false]; //数组的索引前面是number
-console.log(s);
-console.log(s["enabled"]);
-var s2 = [true, false]; //如果 [index:number]这个是number的话，就可以是个数组
-var s23 = { 1: true, 2: false }; //如果 [index:number]这个是number的话，就可以是个数组
-console.log(s2);
-console.log(s2[1]);
-console.log(s2.length);
-var s22 = [1, 2, 3];
-console.log(s22.length);
-var s33 = [true, false, false, true];
-console.log(s33);
-console.log(s33.length);
-console.log(s33.pop());
-var s4 = { mainScreen: true, max: false };
-var example = {
-    color: "red",
-    nest: {
-        ".subclass": {
-            color: "blue",
-            nest: {
-                ".sub": { color: "blue" },
-            },
-        },
+var Todo1 = /** @class */ (function () {
+    function Todo1() {
+    }
+    return Todo1;
+}());
+// let todos: Todo1[]
+var todo = {
+    userId: 1,
+    id: 1,
+    title: "delectus ant fds",
+    completed: false,
+};
+var todos = [
+    {
+        userId: 1,
+        id: 1,
+        title: "delectus ant fds",
+        completed: false,
     },
-};
-var c1 = {
-    color: "Red",
-};
-// root -> tree
+    {
+        userId: 2,
+        id: 2,
+        title: "delectus222 ant fds",
+        completed: true,
+    },
+    {
+        userId: 3,
+        id: 3,
+        title: "delectus3333 ant fds",
+        completed: false,
+    },
+];
--- index.ts
+++ index.ts
@@ -1,76 +1,41 @@
-interface States {
-  [state: string]: boolean; //state可以任意名称，只要类型对应上
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-let s: States = { enabled: true, maximized: false, aaa: "dfd" }; //aaa必须是字符串
-let s1: States = [true, false]; //数组的索引前面是number
-console.log(s);
-console.log(s["enabled"]);
-
-interface State1 {
-  //没有array的方法push,join，length...
-  [index: number]: boolean;
+class Todo1 {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
+// let todos: Todo1[]
 
-let s2: State1 = [true, false]; //如果 [index:number]这个是number的话，就可以是个数组
-let s23: State1 = { 1: true, 2: false }; //如果 [index:number]这个是number的话，就可以是个数组
-console.log(s2);
-console.log(s2[1]);
-console.log(s2.length);
-
-let s22: number[] = [1, 2, 3];
-console.log(s22.length);
-//?这个和interface State1有什么区别
-//1. interface State1可以是对象，可以是数组，比较灵活
-//2. interface State1没有length,s22有length，s2没有array的方法push,join...，s22有
-
-//An index signature parameter type must be 'string', 'number', 'symbol', or a template literal type.
-interface State2 {
-  [index: boolean]: boolean; //index这里的类型永远是 'string', 'number', 'symbol'，不可能是boolean
-}
-
-interface States3 {
-  [index: number]: boolean;
-  length: number;
-  pop(): boolean | undefined;
-}
-
-let s33: States3 = [true, false, false, true];
-console.log(s33);
-console.log(s33.length);
-console.log(s33.pop());
-
-interface State4 {
-  mainScreen: string;
-  [state: string]: boolean;
-  // mainScreen:boolean//必须和上面一样是boolean,否则抱错
-  // mainScreen:strmainScreen:stringing//mainScreen和[state:string]声明的类型一定要相同
-}
-
-let s4: State4 = { mainScreen: true, max: false };
-
-//! 定义一些复杂结构
-interface NestedCss {
-  color?: string;
-  nest?: {
-    [selector: string]: NestedCss;
-  };
-}
-
-let example: NestedCss = {
-  color: "red",
-  nest: {
-    ".subclass": {
-      color: "blue",
-      nest: {
-        ".sub": { color: "blue" },
-      },
-    },
+let todo: Todo = {
+  userId: 1,
+  id: 1,
+  title: "delectus ant fds",
+  completed: false,
+};
+let todos: Todo[] = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus ant fds",
+    completed: false,
   },
-};
-
-let c1: NestedCss = {
-  color: "Red",
-};
-
-// root -> tree
+  {
+    userId: 2,
+    id: 2,
+    title: "delectus222 ant fds",
+    completed: true,
+  },
+  {
+    userId: 3,
+    id: 3,
+    title: "delectus3333 ant fds",
+    completed: false,
+  },
+];
