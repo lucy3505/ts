547cc634af2eb5a4068634715286306a1970d132
lucy3505 <415492767@qq.com>
2023-05-30 15:36:05 +0800

    update

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
-var Pair = /** @class */ (function () {
-    function Pair(first, second) {
-        this.first = first;
-        this.second = second;
-    }
-    return Pair;
-}());
-var pair = new Pair(true, "11");
-console.log(pair.first);
-// Pair<F, S>[]的另一种写法Array<Pair<F,S>>,F[]===Array<F>
-//pairs是个数组参数，数组中的每个元素是Pair<F,S>类型
-function getFirstArray(pairs) {
-    var arr = [];
-    for (var i = 0; i < pairs.length; i++) {
-        var first = pairs[i].first;
-        arr.push(first);
-    }
-    return arr;
+var p = {
+    first: "rails",
+    second: 22,
+};
+var c = {
+    id: Math.random().toString(35),
+    run: function () {
+        return 100;
+    },
+};
+console.log(c.id);
+console.log(c.run());
+function checkElementAt(elements, toBeChecked, atIndex) {
+    return elements[atIndex] === toBeChecked;
 }
-//像Pair<F, S>这个是class，用的时候只要new就可以了
-var numArray = [
-    new Pair(1, true),
-    new Pair(2, false),
-    new Pair(4, true),
-];
-console.log(getFirstArray(numArray));
-function findFirst(items, searchFunction) {
-    for (var i = 0; i < items.length; i++) {
-        var item = items[i];
-        if (searchFunction(item)) {
-            return item;
-        }
-    }
-    return null;
-}
-var items = [1, 2, 3, 4];
-var n = findFirst(items, function (t) { return t % 2 === 0; });
-console.log(n);
-var items2 = ["one", "two", "three"];
-var s = findFirst(items2, function (s) { return s.indexOf("wo") !== -1; });
-console.log(s);
+var checker = checkElementAt;
+var items = [1, 3, 5, 7, 9];
+var b = checker(items, 5, 2);
+console.log(b);
+var g = checker(items, 5, 1);
+console.log(g);
--- index.ts
+++ index.ts
@@ -1,52 +1,43 @@
-class Pair<F, S> {
+interface Pair<F, S> {
   first: F;
   second: S;
-
-  constructor(first, second) {
-    this.first = first;
-    this.second = second;
-  }
 }
 
-let pair = new Pair<boolean, string>(true, "11");
-console.log(pair.first);
+let p: Pair<string, number> = {
+  first: "rails",
+  second: 22,
+};
 
-// Pair<F, S>[]的另一种写法Array<Pair<F,S>>,F[]===Array<F>
-//pairs是个数组参数，数组中的每个元素是Pair<F,S>类型
-function getFirstArray<F, S>(pairs: Pair<F, S>[]): F[] {
-  let arr: F[] = [];
-  for (let i = 0; i < pairs.length; i++) {
-    let first: F = pairs[i].first;
-    arr.push(first);
-  }
-  return arr;
+interface Command<T, R> {
+  id: T;
+  run(): R;
 }
 
-//像Pair<F, S>这个是class，用的时候只要new就可以了
-let numArray: Pair<number, boolean>[] = [
-  new Pair(1, true),
-  new Pair(2, false),
-  new Pair(4, true),
-];
-console.log(getFirstArray(numArray));
+let c: Command<string, number> = {
+  id: Math.random().toString(35),
+  run: function () {
+    return 100;
+  },
+};
+console.log(c.id);
+console.log(c.run());
 
-function findFirst<T>(items: T[], searchFunction: (t: T) => boolean): T | null {
-  for (let i = 0; i < items.length; i++) {
-    let item: T = items[i];
-    if (searchFunction(item)) {
-      return item;
-    }
-  }
-  return null;
+interface ElementChecker {
+  //函数
+  <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
 }
 
-let items: number[] = [1, 2, 3, 4];
-let n: number | null = findFirst(items, (t: number) => t % 2 === 0);
-console.log(n);
+function checkElementAt<T>(
+  elements: T[],
+  toBeChecked: T,
+  atIndex: number
+): boolean {
+  return elements[atIndex] === toBeChecked;
+}
 
-let items2: string[] = ["one", "two", "three"];
-let s: string | null = findFirst<string>(
-  items2,
-  (s: string) => s.indexOf("wo") !== -1
-);
-console.log(s);
+let checker: ElementChecker = checkElementAt;
+let items = [1, 3, 5, 7, 9];
+let b: boolean = checker<number>(items, 5, 2);
+console.log(b);
+let g: boolean = checker<number>(items, 5, 1);
+console.log(g);
