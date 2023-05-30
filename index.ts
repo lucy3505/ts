386a47f396a90e752df562f702748e591f0143e0
lucy3505 <415492767@qq.com>
2023-05-30 09:52:11 +0800

    update

--- index.ts
+++ index.ts
@@ -1,40 +1,72 @@
-//1
-let a: any;
-a = function (): void {
-  console.log("it work");
-};
+//函数名相同，参数不同
+//没有实现的定义
+function sum(x: number, y: number): number;
+function sum(x: number, y: number, z: number): number;
 
-function fun(): any {
-  return function (): void {
-    console.log("it work");
-  };
-}
-//2
-let c: Function;
-c = function (): void {
-  console.log("It work");
-};
-
-//3
-let d: (para: string) => string;
-d = function (pass: string): string {
-  return pass;
-};
-//4
-type e = (para: string) => string;
-const f: e = function (pass: string): string {
-  return pass;
-};
-
-//5
-interface g {
-  (para: string): string;
+//上面两种定义的组合实现
+function sum(x: number, y: number, z?: number): number {
+  // console.log(typeof z);
+  if (typeof z === "undefined") {
+    return x + y;
+  } else {
+    return x + y + z;
+  }
 }
 
-const h: g = (pass: string) => pass;
+sum(1, 2);
+let n = sum(1, 2, 3);
+// console.log(n);
 
-function test(cb: () => string) {
-  let s = cb();
-  return s;
+n = sum(1, 2);
+// console.log(n);
+
+function divide(x: number, y: number): number;
+function divide(str: string, y: number): string[];
+
+function divide(x: any, y: number): any {
+  if (typeof x === "number") {
+    return x / y;
+  } else if (typeof x === "string") {
+    return [x.substring(0, y), x.substring(y)];
+  }
 }
-test(() => "hello");
+
+let n1: number = divide(6, 2);
+console.log(n1);
+
+let s: string[] = divide("football", 4);
+console.log(s);
+
+//class中函数重载
+//静态方法和实例方法
+class Util {
+  divide(x: number, y: number): number;
+  divide(str: string, y: number): string[];
+
+  divide(x: any, y: number): any {
+    if (typeof x === "number") {
+      return x / y;
+    } else if (typeof x === "string") {
+      return [x.substring(0, y), x.substring(y)];
+    }
+  }
+}
+
+let a: Util = new Util();
+console.log(a.divide(6, 2));
+console.log(a.divide("hello world", 4));
+
+class Util2 {
+  static divide(x: number, y: number): number;
+  static divide(str: string, y: number): string[];
+
+  static divide(x: any, y: number): any {
+    if (typeof x === "number") {
+      return x / y;
+    } else if (typeof x === "string") {
+      return [x.substring(0, y), x.substring(y)];
+    }
+  }
+}
+
+let c: number = Util2.divide(6, 2);
