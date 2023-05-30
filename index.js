6efd353ab4c0576cdd76b34bf35c2373565faf71
lucy3505 <415492767@qq.com>
2023-05-30 15:17:28 +0800

    update

--- index.js
+++ index.js
@@ -1,52 +1,41 @@
-var List = /** @class */ (function () {
-    function List(elements) {
-        this.data = elements;
-    }
-    List.prototype.add = function (t) {
-        this.data.push(t);
-    };
-    List.prototype.remove = function (t) {
-        var index = this.data.indexOf(t);
-        if (index > -1) {
-            this.data.splice(index, 1);
-        }
-    };
-    List.prototype.asArray = function () {
-        return this.data;
-    };
-    return List;
-}());
-var numbers = new List([1, 2, 3, 4]);
-numbers.add(5);
-numbers.remove(5);
-var numArray = numbers.asArray();
-console.log(numArray);
-var fruits = new List(["apple", "banana", "orange"]);
-fruits.add("mango");
-fruits.remove("apple");
-var fruitArray = fruits.asArray();
-console.log(fruitArray);
 var Pair = /** @class */ (function () {
     function Pair(first, second) {
-        this._first = first;
-        this._second = second;
+        this.first = first;
+        this.second = second;
     }
-    Object.defineProperty(Pair.prototype, "first", {
-        get: function () {
-            return this._first;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Pair.prototype, "second", {
-        get: function () {
-            return this._second;
-        },
-        enumerable: false,
-        configurable: true
-    });
     return Pair;
 }());
 var pair = new Pair(true, "11");
 console.log(pair.first);
-console.log(pair.second);
+// Pair<F, S>[]的另一种写法Array<Pair<F,S>>,F[]===Array<F>
+//pairs是个数组参数，数组中的每个元素是Pair<F,S>类型
+function getFirstArray(pairs) {
+    var arr = [];
+    for (var i = 0; i < pairs.length; i++) {
+        var first = pairs[i].first;
+        arr.push(first);
+    }
+    return arr;
+}
+//像Pair<F, S>这个是class，用的时候只要new就可以了
+var numArray = [
+    new Pair(1, true),
+    new Pair(2, false),
+    new Pair(4, true),
+];
+console.log(getFirstArray(numArray));
+function findFirst(items, searchFunction) {
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if (searchFunction(item)) {
+            return item;
+        }
+    }
+    return null;
+}
+var items = [1, 2, 3, 4];
+var n = findFirst(items, function (t) { return t % 2 === 0; });
+console.log(n);
+var items2 = ["one", "two", "three"];
+var s = findFirst(items2, function (s) { return s.indexOf("wo") !== -1; });
+console.log(s);
--- index.ts
+++ index.ts
@@ -1,55 +1,52 @@
-class List<T> {
-  private data: T[];
-  constructor(elements: T[]) {
-    this.data = elements;
-  }
-
-  add(t: T) {
-    this.data.push(t);
-  }
-
-  remove(t: T) {
-    let index = this.data.indexOf(t);
-    if (index > -1) {
-      this.data.splice(index, 1);
-    }
-  }
-  asArray(): T[] {
-    return this.data;
-  }
-}
-
-let numbers = new List<number>([1, 2, 3, 4]);
-numbers.add(5);
-numbers.remove(5);
-let numArray = numbers.asArray();
-console.log(numArray);
-
-let fruits = new List<string>(["apple", "banana", "orange"]);
-fruits.add("mango");
-fruits.remove("apple");
-let fruitArray = fruits.asArray();
-console.log(fruitArray);
-
 class Pair<F, S> {
-  private _first: F;
-  private _second: S;
+  first: F;
+  second: S;
 
   constructor(first, second) {
-    this._first = first;
-    this._second = second;
-  }
-
-  get first(): F {
-    return this._first;
-  }
-
-  get second(): S {
-    return this._second;
+    this.first = first;
+    this.second = second;
   }
 }
 
 let pair = new Pair<boolean, string>(true, "11");
 console.log(pair.first);
 
-console.log(pair.second);
+// Pair<F, S>[]的另一种写法Array<Pair<F,S>>,F[]===Array<F>
+//pairs是个数组参数，数组中的每个元素是Pair<F,S>类型
+function getFirstArray<F, S>(pairs: Pair<F, S>[]): F[] {
+  let arr: F[] = [];
+  for (let i = 0; i < pairs.length; i++) {
+    let first: F = pairs[i].first;
+    arr.push(first);
+  }
+  return arr;
+}
+
+//像Pair<F, S>这个是class，用的时候只要new就可以了
+let numArray: Pair<number, boolean>[] = [
+  new Pair(1, true),
+  new Pair(2, false),
+  new Pair(4, true),
+];
+console.log(getFirstArray(numArray));
+
+function findFirst<T>(items: T[], searchFunction: (t: T) => boolean): T | null {
+  for (let i = 0; i < items.length; i++) {
+    let item: T = items[i];
+    if (searchFunction(item)) {
+      return item;
+    }
+  }
+  return null;
+}
+
+let items: number[] = [1, 2, 3, 4];
+let n: number | null = findFirst(items, (t: number) => t % 2 === 0);
+console.log(n);
+
+let items2: string[] = ["one", "two", "three"];
+let s: string | null = findFirst<string>(
+  items2,
+  (s: string) => s.indexOf("wo") !== -1
+);
+console.log(s);
