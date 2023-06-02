49923647e5d67531234790b071bf9f395933bec3
lucy3505 <415492767@qq.com>
2023-06-02 17:55:15 +0800

    update

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function createInstance2(t) {
 }
 var Obj = { a: 1 };
 //  let test:Test=new Test()
-var test2 = createInstance(Obj); //
-console.log(test);
-var test3 = createInstance(Obj); //
-console.log(test);
+// let test2: IObj = createInstance<IObj>(Obj); //
+// console.log(test);
+// let test3: object = createInstance<object>(Obj); //
+// console.log(test);
 function createInstance3(t) {
     var args = [];
     for (var _i = 1; _i < arguments.length; _i++) {
@@ -38,4 +38,21 @@ var Test3 = /** @class */ (function () {
     }
     return Test3;
 }());
-// let test
+var test4 = createInstance3(Test3, 3, 5);
+console.log(test4);
+function createInstance4(constructor) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    //这里的new是要传个class 可以new的,对构造函数的一个约束
+    return new constructor(args);
+}
+var Test4 = /** @class */ (function () {
+    function Test4(x) {
+        this.x = x;
+    }
+    return Test4;
+}());
+var test5 = createInstance3(Test4, 3, 5);
+console.log(test5);
--- index.ts
+++ index.ts
@@ -28,11 +28,11 @@ const Obj = { a: 1 };
 
 //  let test:Test=new Test()
 
-let test2: IObj = createInstance<IObj>(Obj); //
-console.log(test);
+// let test2: IObj = createInstance<IObj>(Obj); //
+// console.log(test);
 
-let test3: object = createInstance<object>(Obj); //
-console.log(test);
+// let test3: object = createInstance<object>(Obj); //
+// console.log(test);
 
 function createInstance3<T>(
   t: new (...constructorArgs: any[]) => T,
@@ -49,4 +49,23 @@ class Test3 {
   }
 }
 
-// let test
+let test4: Test3 = createInstance3(Test3, 3, 5);
+console.log(test4);
+
+function createInstance4<R, T extends { new (...constructorArgs: any[]): R }>(
+  constructor: T,
+  ...args: any[]
+): R {
+  //这里的new是要传个class 可以new的,对构造函数的一个约束
+  return new constructor(args);
+}
+
+class Test4 {
+  private x: number;
+  constructor(x: number) {
+    this.x = x;
+  }
+}
+
+let test5: Test4 = createInstance3(Test4, 3, 5);
+console.log(test5);
