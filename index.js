af4752838670fbe9ffdad9aba53f2562c7848d7d
lucy3505 <415492767@qq.com>
2023-06-02 18:23:34 +0800

    update

--- index.js
+++ index.js
@@ -1,58 +1,14 @@
-function createInstance(t) {
-    //这里的new是要传个class 可以new的,对构造函数的一个约束
-    return new t();
+var numList = { elements: [1, 2, 3] };
+console.log(numList);
+var itemEntity = { id: 1, name: "tail", price: 222 };
+console.log(itemEntity);
+function showPersonContact(personContact) {
+    console.log(personContact);
 }
-var Test = /** @class */ (function () {
-    function Test(x) {
-        this.x = 4;
-        //如果这里用了constructor里面放了参数， 那么new ()=>T 就有问题，因为new ()=>T是没带参数，要带参数就变成new (x:number)=>T
-    }
-    return Test;
-}());
-//  let test:Test=new Test()
-//! class是可以直接当类型使用let test:Test， 正常声明类型使用type,interface，object,number等
-var test = createInstance(Test);
-console.log(test);
-//!
-function createInstance2(t) {
-    //这里的new是要传个class 可以new的,对构造函数的一个约束
-    return new t();
-}
-var Obj = { a: 1 };
-//  let test:Test=new Test()
-// let test2: IObj = createInstance<IObj>(Obj); //
-// console.log(test);
-// let test3: object = createInstance<object>(Obj); //
-// console.log(test);
-function createInstance3(t) {
-    var args = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        args[_i - 1] = arguments[_i];
-    }
-    //这里的new是要传个class 可以new的,对构造函数的一个约束
-    return new t(args);
-}
-var Test3 = /** @class */ (function () {
-    function Test3(x) {
-        this.x = x;
-    }
-    return Test3;
-}());
-var test4 = createInstance3(Test3, 3, 5);
-console.log(test4);
-function createInstance4(constructor) {
-    var args = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        args[_i - 1] = arguments[_i];
-    }
-    //这里的new是要传个class 可以new的,对构造函数的一个约束
-    return new constructor(args);
-}
-var Test4 = /** @class */ (function () {
-    function Test4(x) {
-        this.x = x;
-    }
-    return Test4;
-}());
-var test5 = createInstance3(Test4, 3, 5);
-console.log(test5);
+// &就是把两个类型合一起
+var personContact = { name: "Dane", phone: "111-222-333" };
+showPersonContact(personContact);
+var personDetail = {
+    detail: { name: "Dane", phone: "111-222-333" },
+};
+console.log(personDetail);
--- index.ts
+++ index.ts
@@ -1,71 +1,38 @@
-function createInstance<T>(t: new () => T): T {
-  //这里的new是要传个class 可以new的,对构造函数的一个约束
-  return new t();
+type ListType<T> = { elements: T[] };
+
+let numList: ListType<number> = { elements: [1, 2, 3] };
+console.log(numList);
+
+interface Item {
+  name: string;
+  price: number;
 }
 
-class Test {
-  x: number = 4;
-  constructor(x: number) {
-    //如果这里用了constructor里面放了参数， 那么new ()=>T 就有问题，因为new ()=>T是没带参数，要带参数就变成new (x:number)=>T
-  }
+type Entity<E> = { id: number } & E;
+let itemEntity: Entity<Item> = { id: 1, name: "tail", price: 222 };
+console.log(itemEntity);
+
+interface Person {
+  name: string;
 }
 
-//  let test:Test=new Test()
-//! class是可以直接当类型使用let test:Test， 正常声明类型使用type,interface，object,number等
-let test: Test = createInstance<Test>(Test);
-console.log(test);
-
-//!
-function createInstance2<T>(t: new () => T): T {
-  //这里的new是要传个class 可以new的,对构造函数的一个约束
-  return new t();
+interface Contact {
+  phone: string;
 }
 
-interface IObj {
-  a: number;
-}
-const Obj = { a: 1 };
-
-//  let test:Test=new Test()
-
-// let test2: IObj = createInstance<IObj>(Obj); //
-// console.log(test);
-
-// let test3: object = createInstance<object>(Obj); //
-// console.log(test);
-
-function createInstance3<T>(
-  t: new (...constructorArgs: any[]) => T,
-  ...args: any[]
-): T {
-  //这里的new是要传个class 可以new的,对构造函数的一个约束
-  return new t(args);
+function showPersonContact(personContact: Person & Contact): void {
+  console.log(personContact);
 }
 
-class Test3 {
-  private x: number;
-  constructor(x: number) {
-    this.x = x;
-  }
+// &就是把两个类型合一起
+let personContact: Person & Contact = { name: "Dane", phone: "111-222-333" };
+showPersonContact(personContact);
+
+interface PersonDetail {
+  detail: Person & Contact;
 }
 
-let test4: Test3 = createInstance3(Test3, 3, 5);
-console.log(test4);
-
-function createInstance4<R, T extends { new (...constructorArgs: any[]): R }>(
-  constructor: T,
-  ...args: any[]
-): R {
-  //这里的new是要传个class 可以new的,对构造函数的一个约束
-  return new constructor(args);
-}
-
-class Test4 {
-  private x: number;
-  constructor(x: number) {
-    this.x = x;
-  }
-}
-
-let test5: Test4 = createInstance3(Test4, 3, 5);
-console.log(test5);
+let personDetail: PersonDetail = {
+  detail: { name: "Dane", phone: "111-222-333" },
+};
+console.log(personDetail);
