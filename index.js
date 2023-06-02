098baa7b642bdfdd63e94dfa13baf818513597a0
lucy3505 <415492767@qq.com>
2023-06-02 15:28:53 +0800

    update

--- index.js
+++ index.js
@@ -1,55 +1,33 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
+var A = /** @class */ (function () {
+    function A() {
+        this.x = 5;
+    }
+    return A;
+}());
+var y; //说明y的取值范围只能是A的属性名
+y = "x";
+// y = "xa";//这里就会抱错，A没有这个属性
+var Test = /** @class */ (function () {
+    function Test() {
+        this.x = 6;
+    }
+    return Test;
+}());
+function getProp(a, test) {
+    return test[a];
+}
+var t = new Test();
+var prop = getProp("x", t);
+console.log(prop);
+var B = /** @class */ (function () {
+    function B() {
+    }
+    B.prototype.getAProp = function (a) {
+        return a[this.y];
     };
-    return __assign.apply(this, arguments);
-};
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-function foo(x, y, z) {
-    console.log(x, y, x);
-}
-var args = [0, 1, 2];
-//方法一：
-foo.apply(null, args);
-foo.apply(void 0, args);
-foo.apply(void 0, args);
-//1
-foo.apply(void 0, args); //类型断言
-//2
-function foo1() {
-    var x = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        x[_i] = arguments[_i];
-    }
-    console.log(JSON.stringify(x));
-}
-foo1.apply(void 0, args);
-function foo2(x, b, c) {
-    console.log(x, b);
-}
-foo2.apply(void 0, args);
-foo2(1, 2, 3);
-//destructuring 解构
-var _a = [1, 2, 3, 4], x = _a[0], y = _a[1], remaining = _a.slice(2);
-console.log(x, y, remaining);
-//Array Assignment
-var list = [1, 2];
-list = __spreadArray(__spreadArray([], list, true), [3, 4], false);
-console.log(list);
-//Object spread
-var point2D = { x: 1, y: 2 };
-var point3D = __assign(__assign({}, point2D), { z: 3 });
-console.log(point3D);
+    return B;
+}());
+var b = new B();
+b.y = "x";
+var prop1 = b.getAProp(new A());
+console.log(prop1);
--- index.ts
+++ index.ts
@@ -1,43 +1,32 @@
-function foo(x: number, y: number, z: number) {
-  console.log(x, y, x);
+class A {
+  x: number = 5;
 }
 
-var args: number[] = [0, 1, 2];
+let y: keyof A; //说明y的取值范围只能是A的属性名
+y = "x";
+// y = "xa";//这里就会抱错，A没有这个属性
 
-//方法一：
-foo.apply(null, args);
-foo.apply(void 0, args);
-
-foo(...args);
-//1
-(<any>foo)(...args); //类型断言
-
-//2
-function foo1(...x: number[]) {
-  console.log(JSON.stringify(x));
-}
-foo1(...args);
-
-//3
-function foo2(...x: number[]): void;
-
-function foo2(x: number, b: number, c: number) {
-  console.log(x, b);
+class Test {
+  x: number = 6;
 }
 
-foo2(...args);
-foo2(1, 2, 3);
+function getProp(a: keyof Test, test: Test): any {
+  return test[a];
+}
 
-//destructuring 解构
-var [x, y, ...remaining] = [1, 2, 3, 4];
-console.log(x, y, remaining);
+let t: Test = new Test();
+let prop = getProp("x", t);
+console.log(prop);
 
-//Array Assignment
-var list = [1, 2];
-list = [...list, 3, 4];
-console.log(list);
+class B {
+  y: keyof A;
+  getAProp(a: A): any {
+    return a[this.y];
+  }
+}
 
-//Object spread
-const point2D = { x: 1, y: 2 };
-const point3D = { ...point2D, z: 3 };
-console.log(point3D);
+let b: B = new B();
+b.y = "x";
+
+let prop1 = b.getAProp(new A());
+console.log(prop1);
