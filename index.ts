11e4ba2cc8c84cb89ac3508b8a0632cb11e913ca
lucy3505 <415492767@qq.com>
2023-12-27 16:40:31 +0800

    change

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ class Programmer implements Person {
 const programmer = new Programmer(); //这里没有制定变量programmer的类型，所以没有报错，不受Person接口的限制
 programmer.first_name = "tony";
 
-const programmer2: Person = new Programmer();
+const programmer2: Programmer = new Programmer();
+const programmer3: Person = new Programmer();
 //Cannot assign to 'first_name' because it is a read-only property.
 programmer2.first_name = "tony"; //programmer2的类型声明了是Person，那么Person中的first_Name是readonly,不能修改
+programmer3.first_name = "tony"; //programmer2的类型声明了是Person，那么Person中的first_Name是readonly,不能修改
